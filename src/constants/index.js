f0fbbf76080326baffa5336edff2093dd0d7ac92
Jalal Maqdisi <jalal.maqdisi@gmail.com>
2025-08-23 20:21:08 +0100

    change constant

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,1 +1,1 @@
-export const API_BASE_URL = process.env.NODE_ENV === 'production' ? 'https://abqa.net/api' : 'http://192.168.1.188:8080/api';
+export const API_BASE_URL = process.env.NODE_ENV === 'production' ? 'https://abqa.net/api' : 'http://127.0.0.1:8080/api';
